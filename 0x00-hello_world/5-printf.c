5b40774aa5164cb779c94c81a1d5611c8002a04d
hannahkami <hannahwacheke520@gmail.com>
2023-03-20 09:52:07 -0700

    betty is annoying

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,9 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main - A c program that print a line
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0) ; }
