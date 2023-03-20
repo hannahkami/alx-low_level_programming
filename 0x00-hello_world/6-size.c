a6e8c21cdaee043bbd5051a359ecef5612f0a0b5
hannahkami <hannahwacheke520@gmail.com>
2023-03-20 12:37:05 -0700

    char

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,19 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main - prints a size of a various types of computer type
+ * Return 0
+ */
+int main(void)
+{
+	char f;
+	int g;
+	long int h;
+	long long int i;
+	float j;
+printf("Size of a char: %lu 1 byte(s)\n", (unsigned long)sizeof(f))
+printf("Size of an int: %lu 4 byte(s)\n", (unsigned long)sizeof(g))
+printf("Size of a long int: %lu 4 byte(s)\n", (unsigned long)sizeof(h))
+printf("Size of a long long int: %lu 8 byte(s)\n", (unsigned long)sizeof(i))
+printf("Size of a float: %lu 4 byte(s)\n", (unsigned long)sizeof(j))
+rerurn (0);
+}
