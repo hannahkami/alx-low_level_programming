ec3cbc09d756289876f2d1c9a6a98eb79aaed6af
hannahkami <hannahwacheke520@gmail.com>
2023-03-22 13:23:04 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -15,5 +15,5 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-	_putchar("\n");
+	_putchar('\n');
 	return (0); }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_h
 
-int _putchar(char c);
+int _putchar(char );
 
 
 #endif 
