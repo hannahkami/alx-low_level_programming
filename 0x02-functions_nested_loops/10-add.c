18de0fe9118de4433fe9be71f70d0ee9b5993a5e
hannahkami <hannahwacheke520@gmail.com>
2023-03-22 17:56:05 -0700

    addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - add two numbers
+ * @a: function parameter
+ * @b: function parameter
+ * Return: z
+ *
+ *
+ */
+int add(int a, int b)
+{
+	int z;
+
+	z = a + b;
+return (z); }
