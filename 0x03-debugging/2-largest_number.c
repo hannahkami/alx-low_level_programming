febcdc1e9fd134e4bff3e89a944edbd104314f4f
hannahkami <hannahwacheke520@gmail.com>
2023-03-24 12:00:30 -0700

    largest

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -26,10 +26,9 @@ int largest_number(int a, int b, int c)
 		largest = b;
 	else if (c > a && a > b)
 		largest = c;
-	else if (c > b && b > a)
+	else
 	{
 		largest = c;
 	}
-
 	return (largest);
 }
