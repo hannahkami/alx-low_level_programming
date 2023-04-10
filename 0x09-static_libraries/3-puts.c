85f4abe6ad4e2f7abded7a028e79ce36a6411c74
hannahkami <hannahwacheke520@gmail.com>
2023-04-10 11:41:07 -0700

    static library

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+void _puts(char *str)
+#include<stdio.h>
+/**
+ *_puts - string
+ * *str: string to be printed
+ * Return: Always 0 (Success)
+ */
+void _puts(char *str)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
