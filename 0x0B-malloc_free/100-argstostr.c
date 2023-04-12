efc931ab4526e7b5328087f82e3513d348147ccf
hannahkami <hannahwacheke520@gmail.com>
2023-04-12 14:25:59 -0700

    malloc, free

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -4,7 +4,7 @@
  * argstostr - main entry
  * @ac: int input
  * @av: double pointer array
- * Return: 0
+ * Return: str
  */
 char *argstostr(int ac, char **av)
 {
@@ -31,7 +31,9 @@ char *argstostr(int ac, char **av)
 	}
 	if (str[r] == '\0')
 	{
-		str[r++] = '\n';
+		str[r++] = '\n';	
 	}
 	return (str);
+	}
+	return (0);
 }
